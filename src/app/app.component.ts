376404e00a39d1181c6c6e3c750ad5cf62ba031d
Herberth Barrientos <herberthbarrientos30@gmail.com>
2023-08-15 10:26:36 -0600

    Se modifica el html del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-Crud';
+  title = 'Angular Crud';
+
+  employees=[
+    {'name': 'herberth', position:'programer' },
+    {'name': 'david', position:'designer' },
+    {'name': 'zulma', position:'manager' }
+  ];
+
+  model:any={};
+
+  addEmployee():void{
+
+  }
+
+  deleteEmployee():void{
+
+  }
+
+  editEmployee():void{
+
+  }
+
+  updateEmployee():void{
+
+  }
 }
